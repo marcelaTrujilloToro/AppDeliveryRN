27e417cd074a8a71b190f9508bcdadc68b1770ef
Marcela Trujillo Toro <mt@fullstacksolvers.com>
2023-05-29 18:49:06 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.imageBackground}
+        source={require('./assets/chef.jpg')}
+      />
+
+      <View style={styles.logoContainer}>
+        <Image source={require('./assets/logo.png')} style={styles.logoImage} />
+        <Text style={styles.logoText}>FOOD APP</Text>
+      </View>
+
+      <View style={styles.form}></View>
     </View>
   );
 }
@@ -13,8 +22,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
+  },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+    bottom: '30%',
+  },
+  form: {
+    width: '100%',
+    height: '40%',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  logoContainer: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: '15%',
+  },
+  logoImage: {
+    width: 100,
+    height: 100,
+  },
+  logoText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold',
   },
 });
